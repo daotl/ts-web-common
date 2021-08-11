6ba04cea60fec24b02ca953b2f70ef534ff8dfdf
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-08-11 15:43:35 +0800

    feat: add TSDoc linting

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+/**
+ * Returns greeting to someone.
+ *
+ * @remarks
+ * For more information about this starter template, see: {@link https://github.com/daotl/ts-lib-starter}.
+ *
+ * @param name - Name of someone
+ * @returns Greeting to `name`
+ */
 export function hello(name: string): string {
   return `Hello ${name}!`
 }
