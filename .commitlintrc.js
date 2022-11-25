1e0e294057f3816f6adc4b7b4bf637a5b95e066d
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2022-11-25 14:19:48 +0800

    setup: commitlint

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,28 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [(message) => message.startsWith('WIP: ')],
+  rules: {
+    'type-case': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'WIP',
+        'setup',
+        'build',
+        'docker',
+        'k8s',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+  },
 }
