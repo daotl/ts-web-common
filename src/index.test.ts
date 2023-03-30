61e165db18faa64c68edba8774ae7646129b2380
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2023-03-30 16:40:54 +0800

    setup: switch from ESLint + Prettier to Rome

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,7 @@
 import { hello } from './index.js'
 
+// rome-ignore lint/correctness/noUndeclaredVariables:
 test("hello('Nex') to equal 'Hello Nex!'", () => {
-  expect(hello('Nex')).toBe(`Hello Nex!`)
+  // rome-ignore lint/correctness/noUndeclaredVariables:
+  expect(hello('Nex')).toBe('Hello Nex!')
 })
